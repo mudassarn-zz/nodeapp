a50eea967b2142a63e32009b7ba9017084f4fb13
mudassarn <mudassarn@gmail.com>
2015-07-30 14:20:52 +0500

    Sample EJS app

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var express = require ('express');
+var express = require('express');
+
 var app = express();
 
-app.get('/about', function(req,res) {
-	res.send('<h1>About Us</h1>');
+app.get('/', function(req, res) {
+	res.render('index.ejs', {title:"Hello World"});
+});
+app.get('/about', function(req, res) {
+        res.render('layout.ejs', {title:"About Us",body:"<h1>About Us</h1>"});
 });
 
-app.get('/about/:title', function(req,res) {
-        res.send('<h1>'+req.params.title+'</h1>');
+app.get('/*', function(req, res) {
+        res.status(404).render('error.ejs', {title:"Error"});
 });
-
-app.get('/*', function(req,res) {
-	res.send('<h1>Welcome</h1>');
-});
-
 app.listen(3000);
-console.log("Server started on localhost:3000");
+console.log("Application started on localhost:3000");
